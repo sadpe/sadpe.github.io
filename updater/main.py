2aa459c564fe4738b0720108258cc9fcc5ff6f17
sadpe <69415435+sadpe@users.noreply.github.com>
2021-06-13 08:13:26 -0300

    Update main.py

--- updater/main.py
+++ updater/main.py
@@ -6,6 +6,6 @@ URL = "https://brasil.io/api/dataset/covid19/caso/data/?city=Manaus"
 
 data = requests.get(URL)
 
-if data.status_code == 200:
-    with open('./data/data.json', 'w') as f:
-        f.write(data.text)
+# if data.status_code == 200:
+with open('./data/data.json', 'w') as f:
+    f.write(data.text)
