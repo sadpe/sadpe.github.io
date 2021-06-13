7fda6f89cccd8dba0ef37d5f69d4860101bef54f
sadpe <69415435+sadpe@users.noreply.github.com>
2021-06-13 10:47:35 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@ function build_html(message, city, confirmed, confirmed_diff, date){
 }
 
 request.onload = function(){
-    if (this.status == 200){
+    // if (this.status == 200){
         let z = 'VAZIO'
         let data = JSON.parse(this.response);
         let message = (typeof data['message'] === 'undefined')? z: data['message'];
@@ -30,11 +30,11 @@ request.onload = function(){
         let confirmed_yesterday = (typeof data['results'][1]['confirmed'] === 'undefined')? 0 : data['results'][1]['confirmed'];
         let confirmed_diff = parseInt(confirmed_today) - parseInt(confirmed_yesterday);
         let update_date = (typeof data['results'][0]['date'] === 'undefined')? z : data['results'][0]['date'];
-        let html_text = build_html(message, city, confirmed_today, confirmed_diff, update_date);
+        // let html_text = build_html(message, city, confirmed_today, confirmed_diff, update_date);
         
         // document.body.innerHTML = html_text;
         document.body.innerHTML = data["message"];
-   }
+   // }
 }
 
 request.onerror = function(){
