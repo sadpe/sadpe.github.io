a2de1f4688166eb1851b3da6263de0c22db78598
sadpe <69415435+sadpe@users.noreply.github.com>
2021-06-13 12:16:25 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -34,7 +34,7 @@ request.onload = function(){
         // let html_text = build_html(message, city, confirmed_today, confirmed_diff, update_date);
         
         // document.body.innerHTML = html_text;
-        document.body.innerHTML = String(json_getAllKeys) + '-----';
+        document.body.innerHTML = json_getAllKeys;
    // }
 }
 
