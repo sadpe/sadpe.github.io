2fbb4309615bd0d244c9468f3c345b1c491ce5e7
sadpe <69415435+sadpe@users.noreply.github.com>
2021-06-13 13:41:02 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,18 +4,18 @@ var data_url = 'data/data.json';
 request.open('GET', data_url);
 
 function build_html(message, city, confirmed, confirmed_diff, date){
-    // var text;
-    // text = 'Execução instaurada hoje:'+String(date);
-    // text += String(message);
-    // text += '<br>'
-    // text += '<br>'
-    // text += '<br>'
-    // text += "Cidade: " + String(city);
-    // text += '<br>'
-    // text += `Casos confirmados: ${confirmed} (+${confirmed_diff})`;
-    // text += '<br>'
-    // text += "Data de atualização: " + String(date);
-    return ' ';
+    var text;
+    text = 'Execução instaurada hoje:'+String(date);
+    text += String(message);
+    text += '<br>'
+    text += '<br>'
+    text += '<br>'
+    text += "Cidade: " + String(city);
+    text += '<br>'
+    text += `Casos confirmados: ${confirmed} (+${confirmed_diff})`;
+    text += '<br>'
+    text += "Data de atualização: " + String(date);
+    return text;
 }
 
 request.onload = function(){
@@ -25,14 +25,14 @@ request.onload = function(){
         //let message = (typeof data['message'] === 'undefined' || data['message'] == undefined)? z: data['message'];
         //let city = (data['results'][0]['city'])? True: False;
         let city = ('results' in data && 'city' in data['results'][0])? data['results'][0]['city']: z;
-        //let confirmed_today = (typeof data['results'][0]['confirmed'] === 'undefined')? 0 : data['results'][0]['confirmed'];
-        //let confirmed_yesterday = (typeof data['results'][1]['confirmed'] === 'undefined')? 0 : data['results'][1]['confirmed'];
-        //let confirmed_diff = parseInt(confirmed_today) - parseInt(confirmed_yesterday);
-        //let update_date = (typeof data['results'][0]['date'] === 'undefined')? z : data['results'][0]['date'];
-        // let html_text = build_html(message, city, confirmed_today, confirmed_diff, update_date);
+        let confirmed_today = ('results' in data && 'confirmed' in data['results'][0])? data['results'][0]['confirmed']:z;
+        let confirmed_yesterday = ('results' in data && 'confirmed' in data['results'][1])? data['results'][1]['confirmed']:z;
+        let confirmed_diff = parseInt(confirmed_today) - parseInt(confirmed_yesterday);
+        let update_date = ('results' in data && 'date' in data['results'][0])? data['results'][0]['date']:z;
+        let html_text = build_html(message, city, confirmed_today, confirmed_diff, update_date);
         
-        // document.body.innerHTML = html_text;
-        document.body.innerHTML = city;
+        document.body.innerHTML = html_text;
+        //document.body.innerHTML = city;
    // }
 }
 
