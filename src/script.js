4df6cab91eb9effc9f086f421f6629c9c0ced283
sadpe <69415435+sadpe@users.noreply.github.com>
2021-06-13 17:47:26 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ var data_url = 'data/data.json';
 
 request.open('GET', data_url);
 
-function build_html(hj, message, city, confirmed, confirmed_diff, date){
+function build_html(hj, message,deaths, state, city, confirmed, confirmed_diff, date){
     var text;
     text =  '<b>Execução instaurada hoje: </b>'+hj+'.';
     text += '<br>'
@@ -13,9 +13,11 @@ function build_html(hj, message, city, confirmed, confirmed_diff, date){
     if (date != 'VAZIO'){
         text += '<b>[Atenção] Dados coletados abaixo.</b>'
         text += '<br>'
-        text += '<b>Cidade:</b> '+ String(city);
+        text += '<b>Cidade:</b> '+ String(city) +'-'+String(state);
         text += '<br>'
-        text += `<b>Casos confirmados: </b>${confirmed} (+${confirmed_diff} foram de ontem pra hoje.)`;
+        text += '<b>Óbitos:</b> '+ String(deaths);
+        text += '<br>'
+        text += `<b>Casos confirmados: </b>${confirmed} (+${confirmed_diff} foram últimos registrados.)`;
         text += '<br>'
         text += '<b>Data de atualização: </b>' + String(date);
     }
@@ -31,15 +33,18 @@ request.onload = function(){
         let dayName = new Array ("domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado")
         let monName = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro")
         let now = new Date
-        let hj = dayName[now.getDay() ]+ ","+ now.getDate()+ " de "+monName [now.getMonth()]+  " de "  + now.getFullYear ();
     
+        let hj = dayName[now.getDay() ]+ ","+ now.getDate()+ " de "+monName [now.getMonth()]+  " de "  + now.getFullYear ();
         let message = ('message' in data)? data['message']:z;
+        let deaths = ('results' in data && 'deaths' in data['results'][0])? data['results'][0]['deaths']: z;
+        let state = ('results' in data && 'state' in data['results'][0])? data['results'][0]['state']: z;
+        
         let city = ('results' in data && 'city' in data['results'][0])? data['results'][0]['city']: z;
         let confirmed_today = ('results' in data && 'confirmed' in data['results'][0])? data['results'][0]['confirmed']:z;
         let confirmed_yesterday = ('results' in data && 'confirmed' in data['results'][1])? data['results'][1]['confirmed']:z;
         let confirmed_diff = parseInt(confirmed_today) - parseInt(confirmed_yesterday);
         let update_date = ('results' in data && 'date' in data['results'][0])? data['results'][0]['date']:z;
-        let html_text = build_html(hj, message, city, confirmed_today, confirmed_diff, update_date);
+        let html_text = build_html(hj, message, deaths, state, city, confirmed_today, confirmed_diff, update_date);
         
         document.body.innerHTML = html_text;
         //document.body.innerHTML = city;
